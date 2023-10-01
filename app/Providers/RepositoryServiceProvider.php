ed3b53d1e2cbbe40b6158c71d3cbcded6aa52841
Your Name <you@example.com>
2023-10-01 14:03:45 +0530

    First Commit

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Interfaces\OrderRepositoryInterface;
+use App\Repositories\OrderRepository;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+     }
+
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Order;
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Order::factory()->times(50)->create();
+    }
+}
